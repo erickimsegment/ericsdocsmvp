6ee15efe769e5d94576ac59657b2f0b2977f6737
Eric Kim <31904908+erickimsegment@users.noreply.github.com>
2018-08-02 16:18:09 -0700

    Update BlogPost.vue

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -17,7 +17,7 @@
 
       <footer class="post__footer">
         <vue-disqus v-if="commentsReady" shortname="vue-blog-demo"
-          :key="post" :identifier="post" :url="`https://vue-blog-demo.netlify.com/read/${post}`"/>
+          :key="post" :identifier="post" :url="`https://epic-goldberg-e82a14.netlify.com/read/${post}`"/>
       </footer>
     </article>
   </transition>
