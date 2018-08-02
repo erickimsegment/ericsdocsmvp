408dc88f6919e0967aa687afd1fffdff337ae127
Eric Kim <31904908+erickimsegment@users.noreply.github.com>
2018-08-02 16:49:55 -0700

    Update BlogFooter.vue

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,9 +1,6 @@
 <template>
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
-      <a class="signature" href="https://snipcart.com" target="_blank">
-        <img src="../assets/vue-snip.svg"/>
-      </a>
     </footer>
   </transition>
 </template>
